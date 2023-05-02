287da3b801971696f243e7d2634ab22127479c90
yousef morad <youesfmorad12001@gmail.com>
2023-05-02 13:16:27 +0300

    D.Doc

--- controller/DB.php
+++ controller/DB.php
@@ -3,7 +3,7 @@
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "D.DOC");
+define("DB_NAME", "d.doc");
 // Create database connection
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);   
 ?>
